5d3d22d1568ac110d85a51872c73584aaade9c67
Mark Bui <tvyybui@yahoo.com>
2017-08-04 01:02:33 -0700

    initial commit, built out skeleton of app

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+('hamburger', false),
+('cheese burger', false),
+('bacon burger', false),
+('double burger', false),
+('double cheese burger', false);
