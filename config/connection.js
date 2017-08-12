88a325f7f3eae2afbd5faa8f6cf79f30ea05c5f4
Mark Bui <tvyybui@yahoo.com>
2017-08-11 21:45:26 -0700

    heroku is not loading

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
   } else {
     connection = mysql.createConnection({
-  port: process.env.PORT || 3306,
+  port: 3306,
   host: "localhost",
   user: "root",
   password: "",
